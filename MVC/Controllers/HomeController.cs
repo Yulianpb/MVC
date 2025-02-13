93a045b6949642aeaea114377900ab2a5b82f8d0
yulbe <yulbe@YULIAN>
2025-02-12 19:41:25 -0500

    Agregar archivos de proyecto.

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int num1 = 10;
+            int num2 = 20;
+            ViewBag.Num1 = num1;
+            ViewBag.Num2 = num2;
+            return View(ViewBag);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
